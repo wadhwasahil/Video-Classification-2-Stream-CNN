66963bb9c09a23e0de2a8280ffb4a78f4466210d
stillbreeze <asharjaved7@gmail.com>
2016-03-15 22:29:49 +0530

    irrelevant compulsive minor change

--- utility_scripts/remove_negative_instances.py
+++ utility_scripts/remove_negative_instances.py
@@ -2,26 +2,26 @@ import numpy as np
 import pickle
 import sys,os
 
-# root = "../videos"
-# path = os.path.join(root, "train")
-# deleteList=[]
-# count=0
+root = "../videos"
+path = os.path.join(root, "train")
+deleteList=[]
+count=0
 
-# with open('../dataset/train_data.pickle','rb') as f3:
-# 	train_data=pickle.load(f3)
+with open('../dataset/train_data.pickle','rb') as f3:
+	train_data=pickle.load(f3)
 
-# for vid in train_data.keys():
-# 	if all(v == 0 for v in train_data[vid]):
-# 		deleteList.append(vid)
+for vid in train_data.keys():
+	if all(v == 0 for v in train_data[vid]):
+		deleteList.append(vid)
 
-# print len(deleteList)
+print len(deleteList)
 
-# for i in deleteList:
-# 	if os.path.isfile(path+'/'+i):
-# 		os.remove(path+'/'+i)
-# 		count+=1
+for i in deleteList:
+	if os.path.isfile(path+'/'+i):
+		os.remove(path+'/'+i)
+		count+=1
 
-# print count
+print count
 
 root = "../videos"
 path = os.path.join(root, "test")
